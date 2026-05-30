d8b87caed5f3e83d26b4d14a3e02c2aeb84028d8
farisium <farisiumdigital@gmail.com>
2026-05-30 14:05:39 +0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+  },
+});
