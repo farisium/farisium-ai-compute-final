fecb2a6370bb3d3a811db0e9c2fce7962848410e
farisium <farisiumdigital@gmail.com>
2026-06-02 07:17:38 +0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import { App } from "./App";
 import { LangProvider } from "./i18n";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <LangProvider>
         <App />
+        <Analytics />
       </LangProvider>
     </BrowserRouter>
   </React.StrictMode>,
